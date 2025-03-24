b0046cbe46efc148272a2363625566c85e8537fd
VAIBHAV GUPTA <guptavaibhavg2005@gmail.com>
2025-03-24 16:18:34 +0530

    Add files via upload

---
+++ src/components/Home.jsx
@@ -0,0 +1,18 @@
+import FeaturesSection from "./FeaturesSection";
+import Footer from "./Footer";
+import Hero from "./Hero";
+import Pricing from "./Pricing";
+import SubscriptionSection from "./SubscriptionSection";
+
+function Home() {
+  return (
+    <div>
+      <Hero/>
+      {/* <FeaturesSection/>
+      <Pricing />
+      <SubscriptionSection/> */}
+      <Footer/>
+    </div>
+  );
+}
+export default Home;
