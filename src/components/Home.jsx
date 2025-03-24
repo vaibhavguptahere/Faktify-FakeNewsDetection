fd441c976cc57fab2876d03b84da08f148dca4c4
VAIBHAV GUPTA <mgvg3984@gmail.com>
2025-03-24 16:09:09 +0530

    second commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,9 +8,9 @@ function Home() {
   return (
     <div>
       <Hero/>
-      <FeaturesSection/>
+      {/* <FeaturesSection/>
       <Pricing />
-      <SubscriptionSection/>
+      <SubscriptionSection/> */}
       <Footer/>
     </div>
   );
