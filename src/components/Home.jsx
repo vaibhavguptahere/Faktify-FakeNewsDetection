2e385e9fae1aa209b673443283e67bdedaeeac55
VAIBHAV GUPTA <mgvg3984@gmail.com>
2025-04-05 12:38:04 +0530

    Changes in the news section

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,4 @@
 import FeaturesSection from "./FeaturesSection";
-import Footer from "./Footer";
 import Hero from "./Hero";
 import Pricing from "./Pricing";
 import SubscriptionSection from "./SubscriptionSection";
